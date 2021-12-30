f33419ee5237e3a566eec5541e90825903114637
duyphamvu47 <50992400+duyphamvu47@users.noreply.github.com>
2021-12-30 17:27:36 +0700

    Innit commit

---
+++ Source/DbProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace ORM
+{
+    public class DbProvider
+    {
+        private IDbConnection conntion;
+
+
+        /*-------------------------------------------------------------------*/
+
+        public DbProvider(IDbConnection conntion)
+        {
+   
+        }
+
+        public virtual int ExecuteNonQuery()
+        {
+            return 0;
+        }
+
+        public IDataReader ExecuteReader(String sqlString, Dictionary<string, object> parameters)
+        {
+            return null;
+        }
+    }
+}
---
+++ Source/ISqlStringBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM
+{
+    public interface ISqlStringBuilder
+    {
+        string BuildInsert();
+        string BuildUpdate();
+        string BuildDelete();
+        string BuildSelect();
+    }
+}
