7797b3bc0f5fb97b91088918e34e7b8cfcbcba1a
duyphamvu47 <50992400+duyphamvu47@users.noreply.github.com>
2022-01-01 09:29:28 +0700

    Update DbReader.cs

--- Source/DbReader.cs
+++ Source/DbReader.cs
@@ -4,23 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data;
+using System.Reflection;
 
 namespace ORM
 {
     public class DbReader
     {
+        private static MethodInfo genericGetValue = typeof(DbReader).GetMethods().Where(c => c.Name == "GetValue" && c.IsGenericMethod).First();
         private IDataReader reader;
 
         /*-------------------------------------------------------*/
 
 
         public DbReader(IDataReader reader) {
-
+            this.reader = reader;
         }
 
 
-        public T GetValue<T>(int index){
-            return default(T);
+        public T getValue<T>(int index){
+            object value = reader.GetValue(index);
+
+            if (value == DBNull.Value)
+            {
+                return default(T);
+            }
+
+            return (T)reader.GetValue(index);
+        }
+
+        public static MethodInfo getValueMethod(Type type)
+        {
+            return genericGetValue.MakeGenericMethod(type);
         }
     }
 }
