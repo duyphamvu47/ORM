38611cf3a58e044c6778f3651e4f93a3b5673a41
Hồ Nguyễn Huy Hoàng <18127006@student.hcmus.edu.vn>
2022-01-04 08:52:45 +0700

    Add ColumnAttribute

---
+++ Source/DatabaseAttribute/CollumnAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ORM
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    class CollumnAttribute : Attribute
+    {
+        public string Name { get; private set; }
+        public bool IsAutoGenerate { get; set; }
+
+        public CollumnAttribute(string name)
+        {
+            this.Name = name;
+        }
+
+    }
+}
