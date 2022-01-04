d1126e8ffeefcc4ed077ca74604604df0d29f692
Hồ Nguyễn Huy Hoàng <18127006@student.hcmus.edu.vn>
2022-01-04 08:40:46 +0700

    Fix visual studio solution file, create TableAttribute

---
+++ Source/TableAtrribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ORM
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    class TableAtrribute : Attribute
+    {
+        public string Name { get; private set; }
+
+        public TableAtrribute(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
