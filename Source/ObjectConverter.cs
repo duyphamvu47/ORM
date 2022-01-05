fa99f50265194e0920423d7e8baa08e56d933ef8
Hồ Nguyễn Huy Hoàng <18127006@student.hcmus.edu.vn>
2022-01-05 23:21:18 +0700

    Create type deserialize from db reader

--- Source/ObjectConverter.cs
+++ Source/ObjectConverter.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -26,7 +28,33 @@ namespace ORM
 
         public static Func<object, Dictionary<string, object>> CreateDictionaryGenerator(Type type)
         {
-            return null;
+            var dm = new DynamicMethod((string.Format("Dictionary{0}", Guid.NewGuid())), typeof(Dictionary<string, object>), new[] { typeof(object) }, type, true);
+            ILGenerator il = dm.GetILGenerator();
+            il.DeclareLocal(typeof(Dictionary<string, object>));
+            il.Emit(OpCodes.Nop);
+            il.Emit(OpCodes.Newobj, typeof(Dictionary<string, object>).GetConstructor(Type.EmptyTypes));
+            il.Emit(OpCodes.Stloc_0);
+
+            foreach (var item in type.GetProperties(BindingFlags.Public | BindingFlags.Instance))
+            {
+                string columnName = item.Name;
+                il.Emit(OpCodes.Nop);
+                il.Emit(OpCodes.Ldloc_0);
+                il.Emit(OpCodes.Ldstr, columnName);
+                il.Emit(OpCodes.Ldarg_0);
+                il.Emit(OpCodes.Callvirt, item.GetGetMethod());
+                if (item.PropertyType.IsValueType)
+                {
+                    il.Emit(OpCodes.Box, item.PropertyType);
+                }
+                il.Emit(OpCodes.Callvirt, typeof(Dictionary<string, object>).GetMethod("Add"));
+            }
+            il.Emit(OpCodes.Nop);
+
+            il.Emit(OpCodes.Ldloc_0);
+            il.Emit(OpCodes.Ret);
+            var func = (Func<object, Dictionary<string, object>>)dm.CreateDelegate(typeof(Func<object, Dictionary<string, object>>));
+            return func;
         }
     }
 }
